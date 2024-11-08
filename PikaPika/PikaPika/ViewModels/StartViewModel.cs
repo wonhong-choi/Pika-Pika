579034b5d1e3ebda9c05bc668c44695cbcc4cf49
wonhong <cwh0099@naver.com>
2024-11-09 00:00:42 +0900

    impl App Shutdown
    - Ending App
    - re-locate cancel button in WaitMatchingView

--- PikaPika/PikaPika/ViewModels/StartViewModel.cs
+++ PikaPika/PikaPika/ViewModels/StartViewModel.cs
@@ -4,16 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace PikaPika.ViewModels
 {
     public class StartViewModel : ViewModelBase
     {
         public RelayCommand WaitMatchingCommand { get; }
+        public RelayCommand EndGameCommand { get; }
 
         public StartViewModel(Action navigateToWaitMatching)
         {
             WaitMatchingCommand = new RelayCommand(() => { navigateToWaitMatching?.Invoke(); });
+            EndGameCommand = new RelayCommand(() => { Application.Current.Shutdown(); });
         }
     }
 }
